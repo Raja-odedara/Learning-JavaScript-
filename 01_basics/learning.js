dbffcd97b118c373e87a3d18a84ab8ff92c5cc25
Raja-odedara <rodedara337@gmail.com>
2025-05-24 05:03:08 +0000

    data types

---
+++ 01_basics/learning.js
@@ -0,0 +1,27 @@
+let UserName = "raja odedara";
+let Age = 20;
+let Address = "kinderkheda";
+
+
+console.log(`hello my name is ${UserName} my age is ${Age}, i live in a village named ${Address}.`);
+
+let userInformation = {
+    UserName : "raja odedara",
+    Age : 20,
+    Address : "kinderkheda"
+};
+
+console.table(userInformation);
+
+/* primitive data types
+ 1. string
+ 2. number
+ 3. boolean
+ 4. null
+ 5. undefined
+ 6.symbol*/
+
+/* non-primitive data tpes (refrence)
+ 1. array
+ 2. object
+ 3. function */
